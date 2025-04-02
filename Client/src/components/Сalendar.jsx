ae09ff98c4a4e013bcf3b94f95d70f884aa106cc
trsnbb <trrsnbb@gmail.com>
2025-04-02 23:25:52 +0200

    Lessons

--- Client/src/components/Сalendar.jsx
+++ Client/src/components/Сalendar.jsx
@@ -3,13 +3,35 @@ import "./calendar.css";
 
 const Calendar = () => {
   return (
-    <div className="calendar">
-      <div className="grid">
-        {Array.from({ length: 30 }).map((_, index) => (
-          <div key={index} className="cell"></div>
-        ))}
+    <>
+      <div className='calendar'>
+        <div className='count_lesson'>
+          {["1 пара", "2 пара", "3 пара", "4 пара", "5 пара"].map(
+            (text, index) => (
+              <div key={index} className='lesson_number'>
+                {text}
+              </div>
+            )
+          )}
+        </div>
+        {/* <div className='count_day'>
+          {["21", "22", "23", "24", "25"].map(
+            (text, index) => (
+              <div key={index} className='lesson_number'>
+                {text}
+              </div>
+            )
+          )}
+        </div> */}
+        <div className='calendar_grid'>
+          <div className='grid'>
+            {Array.from({ length: 30 }).map((_, index) => (
+              <div key={index} className='cell'></div>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- Client/src/pages/HomePage/HomePage.jsx
+++ Client/src/pages/HomePage/HomePage.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import "./../../layouts/backround.css";
 import Sidebar from "./../../components/Sidebar.jsx";
 import Calendar from "././../../components/Сalendar.jsx";
+import "./../../layouts/home_style.css";
 
 const HomePage = () => {
   return (
-    <div className="backround">
+    <div className='backround'>
       <Sidebar />
-      <div className="main_content">
-        <div className="calendar_container">
-        <Calendar />
-
+      <div className='main_content'>
+        VVVVV
+        <div className='calendar_container'>
+          
+          <Calendar />
         </div>
       </div>
     </div>
   );
 };
 
-  
 export default HomePage;
