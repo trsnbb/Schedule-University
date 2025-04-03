eae14a30aaf5fc47d83bc1a7b7334609b39a82aa
trsnbb <trrsnbb@gmail.com>
2025-04-03 13:47:06 +0200

    Change day

--- Client/src/components/Сalendar.jsx
+++ Client/src/components/Сalendar.jsx
@@ -3,35 +3,33 @@ import "./calendar.css";
 
 const Calendar = () => {
   return (
-    <>
-      <div className='calendar'>
-        <div className='count_lesson'>
-          {["1 пара", "2 пара", "3 пара", "4 пара", "5 пара"].map(
-            (text, index) => (
-              <div key={index} className='lesson_number'>
-                {text}
-              </div>
-            )
-          )}
+    <div className="calendar">
+      <div className="count_lesson">
+        {["1 пара", "2 пара", "3 пара", "4 пара", "5 пара"].map((text, index) => (
+          <div key={index} className="lesson_number">
+            {text}
+          </div>
+        ))}
+      </div>
+      
+      <div className="calendar_container">
+        <div className="count_day">
+          {["21", "22", "23", "24", "25"].map((text, index) => (
+            <div key={index} className="day_number">
+              {text}
+            </div>
+          ))}
         </div>
-        {/* <div className='count_day'>
-          {["21", "22", "23", "24", "25"].map(
-            (text, index) => (
-              <div key={index} className='lesson_number'>
-                {text}
-              </div>
-            )
-          )}
-        </div> */}
-        <div className='calendar_grid'>
-          <div className='grid'>
+
+        <div className="calendar_grid">
+          <div className="grid">
             {Array.from({ length: 30 }).map((_, index) => (
-              <div key={index} className='cell'></div>
+              <div key={index} className="cell"></div>
             ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
